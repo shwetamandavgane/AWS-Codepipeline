4221cac5e4ccddada3fa49c4e0bdbf4d31e176d1
shwetamandavgane <shwetamandavgane@gmail.com>
2020-06-29 18:48:32 -0700

    Made a change to test codedeploy

--- codepipeline/src/main/java/com/aws/codepipelinedemo/codepipeline/HelloWorldController.java
+++ codepipeline/src/main/java/com/aws/codepipelinedemo/codepipeline/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
       @GetMapping("/hello")
       public String displayWelcomeMessage(){
-          return "Welcome to the world of AWS and SpringBoot!!";
+          return "CodeDeploy World - Welcome to the world of AWS and SpringBoot!!";
       }
 }
